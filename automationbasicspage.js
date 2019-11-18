1e1d9da311ff1ce2ce608c1bc9c30a7db16be722
Meheza <mehezatchalim@gmail.com>
2019-11-18 00:44:17 -0700

    Add files via upload

---
+++ automationbasicspage.js
@@ -0,0 +1,20 @@
+module.exports = {
+    url: 'https://devmountain-qa.github.io/Automation-Basics/build/',
+    elements: {
+        button: 'button.confirmationButton',  
+        inputline: 'input.inputLine',
+        evenfield: 'span.resultsBox',
+        oddfield:  'span.resultsBox',
+        palindrome: '[name="palindromeInput"]',
+        palindrombutton: '[name="palindromeButton"]',
+        palidromfield: '[name="palindromeResults"]',
+        suminput: '[name="sumInput1"]',
+        suminputb: '[name="sumInput2"]',
+        sumbutton: '[name="sumButton"]',
+        sumb: '[name="sumResults"]'
+        
+    },
+
+}
+
+
